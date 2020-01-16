636cb1ec9556c9ad3557b9cb5ae313a2ea080234
ryuryukj6663@gmail.com <ryuryukj6663@gmail.com>
2020-01-16 17:00:10 +0900

    Version:2

--- Three_Normalization/Train_model.py
+++ Three_Normalization/Train_model.py
@@ -35,16 +35,20 @@ class Train_model:
             end_batch = i * mini_batch_size
             use_model._partial_fit(train_X[current_batch:end_batch], train_Y[current_batch:end_batch], classes=classes_y)
             current_batch = end_batch
-
         use_model._partial_fit(train_X[current_batch:batch_size], train_Y[current_batch:batch_size], classes=classes_y)
+        pred = use_model.predict(test_X)
+
+        '''
         use_model_score = cross_val_score(use_model, test_X, test_Y, scoring='accuracy', cv=10, n_jobs=8)
         use_model_cross_val = cross_val_predict(use_model, test_X, test_Y, cv=10, n_jobs=8)
-        use_model_confusion_matrix = confusion_matrix(test_Y, use_model_cross_val)
+        '''
 
-        return use_model_cross_val, use_model_score, use_model_confusion_matrix
+        use_model_confusion_matrix = confusion_matrix(test_Y, pred)
+
+        return use_model_confusion_matrix
 
 Nin = 49                # 입력 노드의 개수
-Nh_l = [1000, 500, 100]        # 히든 레이어 개수
+Nh_l = [1000, 500, 100] # 히든 레이어 개수
 number_of_class = 15    # 분류 클래스 개수
 Nout = number_of_class  # 출력 노드의 개수
 
@@ -67,8 +71,8 @@ class DNN(models.Sequential):
         train_X = np.array(train_X)
         test_X = np.array(test_X)
 
-        print(train_X.shape)
-        print(test_X.shape)
+        print(train_X.shape)        # (samples, 49)
+        print(test_X.shape)         # (samples, 49)
 
         a, b = train_X.shape     # 2D shape를 가진다.
         a1, b1 = test_X.shape
@@ -78,9 +82,6 @@ class DNN(models.Sequential):
 
         train_X = np.array(train_X)
         test_X = np.array(test_X)
-
-        print(train_X.shape)
-        print(test_X.shape)
 
         ## Count Y_Label
         train_Y_cnt = Counter(train_Y)
@@ -105,50 +106,16 @@ class DNN(models.Sequential):
         print('Attack Mapping : {}'.format(attack_mapping))
 
         ## One-Hot
-        re = to_categorical(y_train, num_classes=output_size)
-        re2 = to_categorical(y_test, num_classes=output_size)
+        train_Y2 = to_categorical(y_train, num_classes=output_size)
+        test_Y2 = to_categorical(y_test, num_classes=output_size)
         print('Original Data : {}'.format(train_Y))
         print('Original Data : {}'.format(test_Y))
-        print('\nOne-Hot Result from Y_Train : \n{}'.format(re))
-        print('\nOne-Hot Result from Y_Train : \n{}'.format(re2))
+        print('\nOne-Hot Result from Y_Train : \n{}'.format(train_Y2))
+        print('\nOne-Hot Result from Y_Test : \n{}'.format(test_Y2))
 
-        '''
-        train_cnt_Y = Counter(train_Y)
-        test_cnt_Y = Counter(test_Y)
-        classes_y = len(train_cnt_Y.keys())
-        print('Attack Type Count : {}\n'.format(dict(train_cnt_Y)))
-        print('Attack Type Count : {}'.format(dict(test_cnt_Y)))
-
-
-        # Label(Type : String -> Float)
-        l_encoder = LabelEncoder()
-        l_encoder2 = LabelEncoder()
-        train_Y = l_encoder.fit(train_Y)
-        test_Y = l_encoder2.fit(test_Y)
-        # print('train', train_Y)
-        # print('test', test_Y)
-
-        key = ['BENIGN', 'DDoS', 'DoS GoldenEye', 'Heartbleed', 'PortScan', 'Bot', 'FTP-Patator', 'DoS Hulk', 'Web Attack XSS', 'DoS slowloris', 'Web Attack Sql Injection', 'Web Attack Brute Force', 'DoS Slowhttptest', 'Infiltration', 'SSH-Patator']
-
-        value = l_encoder.transform(key)
-        value2 = l_encoder2.transform(key)
-        attack_maping = dict(zip(key, value))
-        attack_maping2 = dict(zip(key, value2))
-        print('Attack Mapping : {}'.format(attack_maping))
-        print('Attack Mapping2 : {}'.format(attack_maping2))
-
-        onehot_encoder = OneHotEncoder()
-
-        train_Y = onehot_encoder.fit_transform(train_Y)
-        test_Y = onehot_encoder.fit_transform(test_Y)
-
-        # train_Y = to_categorical(train_Y, num_classes=classes_y)
-        # test_Y = to_categorical(test_Y, num_classes=classes_y)
-        print('one-hot encoding : \n train_Y {} \n test_Y {}'.format(train_Y, test_Y))
-        '''
         use_model = DNN(Nin, Nh_l, Nout)
-        history = use_model.fit(train_X, train_Y, epochs=100, batch_size=10000,
+        history = use_model.fit(train_X, train_Y2, epochs=100, batch_size=10000,
                                 validation_split=0.2, verbose=1)
-        performance_test = use_model.evaluate(test_X, test_Y, batch_size=10000)
+        performance_test = use_model.evaluate(test_X, test_Y2, batch_size=10000)
 
         return performance_test, history
--- Three_Normalization/__main__.py
+++ Three_Normalization/__main__.py
@@ -10,8 +10,6 @@ from Three_Normalization.Train_model import Train_model as tm
 from Three_Normalization.Train_model import DNN as dnn
 from Three_Normalization.Drawing import Drawing as dw
 
-
-
 class Main:
     if __name__ == "__main__":
         features = ['Destination Port', 'Flow Duration', 'Total Fwd Packets', 'Total Backward Packets', 'Total Length of Fwd Packets', 'Total Length of Bwd Packets', 'Fwd Packet Length Max', 'Fwd Packet Length Min', 'Fwd Packet Length Mean', 'Fwd Packet Length Std', 'Bwd Packet Length Max', 'Bwd Packet Length Min', 'Bwd Packet Length Mean', 'Bwd Packet Length Std', 'Flow Bytes/s', 'Flow Packets/s', 'Flow IAT Mean', 'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Total', 'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Total', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max', 'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags', 'Bwd URG Flags', 'Fwd Header Length', 'Bwd Header Length', 'Fwd Packets/s', 'Bwd Packets/s', 'Min Packet Length', 'Max Packet Length', 'Packet Length Mean', 'Packet Length Std', 'Packet Length Variance', 'FIN Flag Count', 'SYN Flag Count', 'RST Flag Count', 'PSH Flag Count', 'ACK Flag Count', 'URG Flag Count', 'CWE Flag Count', 'ECE Flag Count', 'Down/Up Ratio', 'Average Packet Size', 'Avg Fwd Segment Size', 'Avg Bwd Segment Size', 'Fwd Header Length', 'Fwd Avg Bytes/Bulk', 'Fwd Avg Packets/Bulk', 'Fwd Avg Bulk Rate', 'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk', 'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', 'Subflow Fwd Bytes', 'Subflow Bwd Packets', 'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'Init_Win_bytes_backward', 'act_data_pkt_fwd', 'min_seg_size_forward', 'Active Mean', 'Active Std', 'Active Max', 'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min', 'Label']
@@ -25,36 +23,37 @@ class Main:
         data, data_Label = cl.data_cleaning(data)
         classes_y = np.unique(data_Label)
 
-        new_data = sb.sampling_1(data, data_Label, features)
-        # new_data = sb.sampling_2(data, data_Label, features)
-        new_data = pd.DataFrame(new_data)
-        print(new_data)
-        bal_data_Label, bal_data = new_data['Label'], new_data.drop('Label', 1)
+        sampling_strategy = ['1', '2']
 
-        fe = fe.feature_extraction_1(bal_data, bal_data_Label, features)
+        for i in range(1, len(sampling_strategy)):
+            if i == 1:
+                new_data = sb.sampling_1(data, data_Label, features)
+            elif i == 2:
+                new_data = sb.sampling_2(data, data_Label, features)
 
-        normalization_type = ['mms', 'std', 'qnt']
+            new_data = pd.DataFrame(new_data)
+            print(new_data)
+            bal_data_Label, bal_data = new_data['Label'], new_data.drop('Label', 1)
 
-        for norm in range(0, len(normalization_type)):
-            norm_data = dn.normalizations(fe, normalization_type[norm])
-            train_X, train_Y, test_X, test_Y, raw_encoded, raw_cat = ds.data_split(norm_data)
-            performance_test, history = dnn.dnn_model(train_X, train_Y, test_X, test_Y)
-            print('Test Loss and Accuracy ->', performance_test)
+            fe = fe.feature_extraction_1(bal_data, bal_data_Label, features)
 
-            dw.drawplot(history)
-        '''
-        for norm in range(0, len(normalization_type)):
-            norm_data = dn.normalizations(fe, normalization_type[norm])
-            train_X, train_Y, test_X, test_Y, raw_encoded, raw_cat = ds.data_split(norm_data)
-            use_model_cross_val, use_model_score, use_model_confusion_matrix = tm.train_model(train_X, train_Y,
-                                                                                              classes_y, test_X,
-                                                                                              test_Y)
-            report = classification_report(test_Y, use_model_cross_val, target_names=raw_cat)
-        
-            # 그래프 그리기(confusion matrix, histogram)
-            dw.print_confusion_matrix(use_model_confusion_matrix, classes_y)
-            # dw.print_score_graph(use_model_score, index=classes_y, columns=classes_y)
+            normalization_type = ['mms', 'std', 'qnt']
+            for norm in range(0, len(normalization_type)):
+                norm_data = dn.normalizations(fe, normalization_type[norm])
+                train_X, train_Y, test_X, test_Y, raw_encoded, raw_cat = ds.data_split(norm_data)
+                use_model_confusion_matrix = tm.train_model(train_X, train_Y, classes_y, test_X, test_Y)
+                # report = classification_report(test_Y, use_model_cross_val, target_names=raw_cat)
 
-            print(str(report))
-        '''
+                # 그래프 그리기(confusion matrix, histogram)
+                dw.print_confusion_matrix(use_model_confusion_matrix, classes_y)
+                # dw.print_score_graph(use_model_score, index=classes_y, columns=classes_y)
 
+                # print(str(report))
+
+            for norm in range(0, len(normalization_type)):
+                norm_data = dn.normalizations(fe, normalization_type[norm])
+                train_X, train_Y, test_X, test_Y, raw_encoded, raw_cat = ds.data_split(norm_data)
+                performance_test, history = dnn.dnn_model(train_X, train_Y, test_X, test_Y)
+                print('Test Loss and Accuracy ->', performance_test)
+
+                # dw.drawplot(history)
